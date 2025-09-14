b0efcb0236a6c28fb942d7f0403964ad0b3dbe4e
Darwis626 <dharwiiisdar@gmail.com>
2025-09-14 21:19:42 +0800

    Day.java

---
+++ Day.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main ( String [] args){
+        char angka = '6';
+        boolean isBenar = true ;
+        
+        System.out.println("karakter huruf:" + angka");
+        System.out.println("Nilai isBenar:" + isBenar");
+    }
+}
