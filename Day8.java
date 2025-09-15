e0cb73dc702bd43b1f3adad30154deee2e20be34
Darwis626 <dharwiiisdar@gmail.com>
2025-09-15 22:05:44 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+ppublic class Main {
+    public static void main ( String [] args){
+        String Nama = "Darwis";
+        System.out.println("Nama saya" + Nama);
+        String Umur = "18";
+        System.out.println("Umur saya:" + Umur);
+        String Salam = "Hallo Semua";
+        System.out.println("Kata Salam" + Salam);
+    }
+}
+        
