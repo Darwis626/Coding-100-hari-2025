24e2e7df79943bdabe7bfae1c42b4ed1e9c3b610
Darwis626 <dharwiiisdar@gmail.com>
2025-09-16 22:42:57 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main ( String [] args){
+        int angkaUmur = 18;
+        double tahunLahir = 2007;
+        String kataSalam = "Hallo";
+        
+        System.out.println("Angka pertama: " + angkaUmur);
+        System.out.println("Tahun Lahir: " + tahunLahir);
+        System.out.println("Kata Salam: " + kataSalam);
+        
+        String angkaUmur = 18;
+        String tahunLahir = 07;
+        String kataSalam = "Hallo";
+        
+        System.out.println("Angka Umur: " + angkaUmur);
+        System.out.println("Tahun Lahir: " + tahunLahir);
+        System.out.println("Kata Salam: " + kataSalam);
+    }
+}
