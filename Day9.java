8f22e57c418ee031ffedb0937d5ba1f99f29f299
Darwis626 <dharwiiisdar@gmail.com>
2025-09-17 19:48:23 +0800

    Day9.java

--- Day9.java
+++ Day9.java
@@ -1,23 +1,18 @@
 public class Main {
     public static void main ( String [] args){
-        // Deklarasi //
-        int nama;
-        long panjang;
         
-        //Insialisasi //
-        int angka = 50;
-        String nama = "Darwis";
+        String nama;
+        int umur
         
-        // Menampilkan nilai awal //
-        System.out.println("Nilai awal angka:" + angka);
-        System.out.println("Nilai awal nama:" + nama);
+      
+        String nama="Darwis";
+        int umur=18;
+        System.out.println("Nilai sebelum update"+nama);
+        System.out.println("Nilai sebelum update"+umur);
         
-        // Update variabel //
-        tinggi = 166;
-        berat = 50;
-        
-        // Nilai variabel setelah di update //
-        System.out.println("Nilai umur setelah di update: " + tinggi);
-        System.out.println("Nilai berat setelah di update: " + berat);
+        nama="awi";
+        umur=18;
+        System.out.println("Nilai setelah di update"+nama);
+        System.out.println("Nilai setelah di update"+umur);
     }
 }
