9947803070e06db87fdc381f7c24aef676f36731
Darwis626 <dharwiiisdar@gmail.com>
2025-09-17 20:36:01 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main ( String [] args){
+        
+        int umur =18;
+        final String nama = "Darwis"
+        
+        System.out.println("Umur" + umur);
+        System.out.println("finafl" + nama);
+    }
+}
