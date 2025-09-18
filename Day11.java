95654c017a8af4cb4a1a42647bb7c2e28aac1be5
Darwis626 <dharwiiisdar@gmail.com>
2025-09-18 22:48:06 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,11 +1,12 @@
-import java.util.Scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main ( String [] args){
+    public static void main(String[] args) {
         
-        Scanner imput = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);
         
         System.out.print("Masukkan nama anda: ");
-        String nama = input nextLine();
-        System.out.println("nama");
+        String nama = input.nextLine();
+        System.out.println("nama: " + nama);
     }
 }
