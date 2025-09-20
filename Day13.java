6ba4d9ff23f77cea21ffcfb18cb5e85395c8d074
Darwis626 <dharwiiisdar@gmail.com>
2025-09-20 22:28:12 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,13 @@
+public class Main  {
+
+    public static void main(String[] args) {
+       
+        String nama = "Darwis";
+        int umur = 18;
+        double ipk = 3.79;
+        
+        System.out.println("Nama saya: " + nama);
+        System.out.println("Umur saya: " + umur);
+        System.out.printf("Nama : %s   Umur : %d   IPK : %.2f\n", nama, umur, ipk);
+    }
+}
