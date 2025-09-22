8f9035cbfed4652239bebafaf6d3f16c66e362a2
Darwis626 <dharwiiisdar@gmail.com>
2025-09-22 23:18:02 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+public class Main  {
+
+    public static void main(String[] args) {
+       
+      int angka1 =35;
+       int angka2 =15;
+       
+          int hasilpenjumlahan = angka1 + angka2;
+          int hasilpengurangan = angka1- angka2;
+          
+          System.out.println("hasilpenjumlahan= " + hasilpenjumlahan);
+          System.out.println("hasilpengurangan= " + hasilpengurangan);
+          
+       
+             
+                   
+                       }
+                       }    
