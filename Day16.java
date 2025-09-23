d9fcd3a1dc8a88ca86a79ce8e647348c43067b9e
Darwis626 <dharwiiisdar@gmail.com>
2025-09-23 23:03:32 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,19 @@
+public class Main  {
+
+    public static void main(String[] args) {
+       
+      int angka1 =20;
+       int angka2 =10;
+       
+          int hasilperkalian = angka1 * angka2;
+          int hasilpembagian = angka1 / angka2;
+          
+          System.out.println("hasilperkalian = " + hasilperkalian);
+          System.out.println("hasilpembagian = " + hasilpembagian);
+          
+       
+             
+                   
+                       }
+                       }    
+                       
