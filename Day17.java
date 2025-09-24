2714dd86c3d0b4a8e9de5f2660280f8b11b44ec6
Darwis626 <dharwiiisdar@gmail.com>
2025-09-24 21:37:40 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+    	int angka1 = 10;
+		int angka2 = 3;
+		int angka3 = angka1 % angka2;
+		System.out.println("10 % 3 = "+angka3);
+		
+		
+	}
+}
+         
