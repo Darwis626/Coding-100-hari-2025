061e19f6e826f87c44375c609e130e83ce16a4a7
Darwis626 <dharwiiisdar@gmail.com>
2025-09-25 22:20:24 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println("byteValue: " + byteValue);
+		System.out.println("int value setelah konverensi dari byte: " + intValue);
+		
+	}
+}
+         
