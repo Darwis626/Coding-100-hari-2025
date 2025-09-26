3bfc48e786b68ce211f355b5e503d61110460da2
Darwis626 <dharwiiisdar@gmail.com>
2025-09-26 20:51:29 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+		double myDouble = 7.26;
+		int myInt = (int) myDouble;
+		
+		System.out.println("Nilai double:" + myDouble);
+		System.out.println("Nilai int: " + myInt);
+		
+	
+	}
+}
+         
