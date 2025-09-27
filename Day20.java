95bbee9122a19493731d0fb85c1fbb74a5a43578
Darwis626 <dharwiiisdar@gmail.com>
2025-09-27 22:12:36 +0800

    Day20.java

--- Day20.java
+++ Day20.java
@@ -1,8 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
-	    
-	int angka = 123;
-	String hasilString = String.valueOf(angka);
-	System.out.println("hasil: " + hasilString);
+	 
+	 // metode pertama   
+	int a = 123;
+	String aString = String.valueOf(a);
+	System.out.println(aString);
+	
+	// metode kedua
+	float b = 'A';
+	String bString = Float.toString(b);
+	System.out.println(bString);
+	
+	// metode ketiga
+	double c = 2;
+	String cString = c + "";
+	System.out.println(cString);
+	
+
 	}
 }
