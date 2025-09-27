491accefd27a61cd21bc67e28cc9ad4368bd328c
Darwis626 <dharwiiisdar@gmail.com>
2025-09-27 17:23:05 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+	    
+	int angka = 123;
+	String hasilString = String.valueOf(angka);
+	System.out.println("hasil: " + hasilString);
+	}
+}
