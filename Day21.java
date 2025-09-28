378c26dc18d0031069fbf09e2750bec4417c462d
Darwis626 <dharwiiisdar@gmail.com>
2025-09-28 22:59:37 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,25 @@
+public class Main {
+	public static void main(String[] args) {
+	  
+	  int a = 5;
+	  int b = 10;
+	  
+	  System.out.println("sebelum di tukar : a = " + a +", b = + b");
+	
+	 // langkah 1 :
+	  a = a + b;
+	  // lsngkah 2 :
+	  b = a - b;
+	  // lahkah 3 :
+	  a = a - b;
+	  
+	  System.out.println("setelah di tukar : a = " + a + " , b = " + b);
+	 
+	  
+	    
+        
+
+
+	}
+  }
+         
