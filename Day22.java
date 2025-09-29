05d921e200447e0189d22023e2508fc3b1c80cc2
Darwis626 <dharwiiisdar@gmail.com>
2025-09-29 21:07:00 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double sisi = 7.0;
+        double luas = sisi * sisi;
+         
+       System.out.println("luas persegi adalah : " + luas);
+     
+		
+    }
+}
