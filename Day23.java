3a7d7a169a5f058a5f6254f6305b6e415683c9e8
Darwis626 <dharwiiisdar@gmail.com>
2025-09-30 00:22:59 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int panjang = 5;
+        int lebar = 10;
+        int luas = panjang * lebar;
+         
+       System.out.println("luas persegi panjang adalah : " + luas);
+     
+		
+    }
+}
