ea635fe30dced3f4db48598bf4a362f2b3be1e67
Darwis626 <dharwiiisdar@gmail.com>
2025-10-01 21:48:13 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double jarijari = 3.5;
+        double luas = Math.PI * jarijari * jarijari;
+        
+        System.out.println("Luas lingkaran adalah : " + luas);
+     
+		
+    }
+}
